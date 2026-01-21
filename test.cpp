67c8572c30aca7661463d4f3efff676327b11791
Rabeya Akter <rabeyariya251@gmail.com>
2026-01-21 17:40:19 +0600

    solve 3 easy problem from hacker rank and learn new header file called input output manupulator <iomanip> wich is used for control the output and it it used with cout a

--- test.cpp
+++ test.cpp
@@ -1,6 +1,50 @@
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 int main()
 {
-    printf("Hello");
+    int n = 10;
+    int count = 0;
+    int sum = 0;
+
+    int a;
+    long l;
+    char ch;
+    float f;
+    double d;
+
+    cin >> a;
+    cin >> l;
+    cin >> ch;
+    cin >> f;
+    cin >> d;
+
+    cout << a << endl;
+    cout << l << endl;
+    cout << ch << endl;
+    cout << fixed << setprecision(3) << f << endl;
+    cout << fixed << setprecision(9) << d << endl;
+
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     if (i % 2 != 0)
+    //     {
+    //         sum = sum + i;
+    //     }
+    // };
+
+    while (count <= n) // condition
+    {
+        if (count % 2 != 0) // kaj kore
+        {
+            sum = sum + count;
+        }
+
+        count++; // update kore
+    };
+
+    // cout << "Sum:" << sum << endl;
     return 0;
 }
+
+// g++ -std=c++17 test.cpp -o test && ./test.exe
