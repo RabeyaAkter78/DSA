c6c9cca84530d93227bbd2f716ae518f483670d7
Rabeya Akter <rabeyariya251@gmail.com>
2026-02-18 16:22:53 +0600

    practice data types probleem

--- loop.cpp
+++ loop.cpp
@@ -3,63 +3,106 @@ using namespace std;
 
 int main()
 {
-    int start;
-    int end;
-    cout << "Enter the  start number :" << endl;
-    cin >> start;
-    cout << "Enter the end number :" << endl;
-    cin >> end;
+    // int a=8;
+    // int b=11;
 
-    for (int i = start; i <= end; i++)
+    // for (int i = a; i <= b; i++)
+    // {
+    //     if (i <= 9)
+    //     {
+    //         if (i == 1)
+    //         {
+    //             cout << "one" << endl;
+    //         }
+    //         else if (i == 2)
+    //         {
+    //             cout << "two" << endl;
+    //         }
+    //         else if (i == 3)
+    //         {
+    //             cout << "three" << endl;
+    //         }
+    //         else if (i == 4)
+    //         {
+    //             cout << "four" << endl;
+    //         }
+    //         else if (i == 5)
+    //         {
+    //             cout << "five" << endl;
+    //         }
+    //         else if (i == 6)
+    //         {
+    //             cout << "six" << endl;
+    //         }
+    //         else if (i == 7)
+    //         {
+    //             cout << "seven" << endl;
+    //         }
+    //         else if (i == 8)
+    //         {
+    //             cout << "eight" << endl;
+    //         }
+    //         else if (i == 9)
+    //         {
+    //             cout << "nine" << endl;
+    //         }
+    //     }
+    //     else if (i % 2 == 0)
+    //     {
+    //         cout << "even" << endl;
+    //     }
+    //     else
+    //     {
+    //         cout << "odd" << endl;
+    //     }
+    // }
+
+    // return 0;
+
+    int n;
+    cin >> n;
+    if (n <= 9)
     {
-        if (i <= 9)
+        if (n == 1)
         {
-            if (i == 1)
-            {
-                cout << "one" << endl;
-            }
-            else if (i == 2)
-            {
-                cout << "two" << endl;
-            }
-            else if (i == 3)
-            {
-                cout << "three" << endl;
-            }
-            else if (i == 4)
-            {
-                cout << "four" << endl;
-            }
-            else if (i == 5)
-            {
-                cout << "five" << endl;
-            }
-            else if (i == 6)
-            {
-                cout << "six" << endl;
-            }
-            else if (i == 7)
-            {
-                cout << "seven" << endl;
-            }
-            else if (i == 8)
-            {
-                cout << "eight" << endl;
-            }
-            else if (i == 9)
-            {
-                cout << "nine" << endl;
-            }
+            cout << "one" << endl;
         }
-        else if (i % 2 == 0)
+        else if (n == 2)
         {
-            cout << "even" << endl;
+            cout << "two" << endl;
         }
-        else
+        else if (n == 3)
         {
-            cout << "odd" << endl;
+            cout << "three" << endl;
+        }
+        else if (n == 4)
+        {
+            cout << "four" << endl;
+        }
+        else if (n == 5)
+        {
+            cout << "five" << endl;
+        }
+        else if (n == 6)
+        {
+            cout << "six" << endl;
+        }
+        else if (n == 7)
+        {
+            cout << "seven" << endl;
+        }
+        else if (n == 8)
+        {
+            cout << "eight" << endl;
+        }
+        else if (n == 9)
+        {
+            cout << "nine" << endl;
         }
     }
-
+    else
+    {
+        cout << "Greater than 9" << endl;
+    }
     return 0;
 }
